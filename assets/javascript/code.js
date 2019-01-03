46f74bff175a859347fd8d18f5390d878885333d
Christopher Donner <christopher.donner@gmail.com>
2019-01-03 16:49:38 -0500

    auth kindof working

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,16 +1,10 @@
+var authToken="c.s7K5ndoFFMrwvCbFypU2U29sRxlTQZ4jSJTvWD6zHLOxuRJjznVkm38MJi204sQrJnFhvQn3pygTeoM8IFLpLZfQwSlXE47zzs6JrPzGHW3X4FRW0FqNlbOi9sUYeBfFMnLePx1Gjfqn8MgS"
+
 $.ajax({
-    url: "https://developer-api.nest.com",
-    method: "GET",
-    // Our data is the FormData object we set up above.
-    //data: fd,
+    url: `https://developer-api.nest.com/?auth=${authToken}`,
+    type: "GET",
     contentType: "application/json",
-    //processData: false,
-    //dataType: 'json',
-    //contentType: 'application/json; charset=utf-8',
-    beforeSend: function(request) {
-       request.setRequestHeader( "authorization", "Bearer c.s7K5ndoFFMrwvCbFypU2U29sRxlTQZ4jSJTvWD6zHLOxuRJjznVkm38MJi204sQrJnFhvQn3pygTeoM8IFLpLZfQwSlXE47zzs6JrPzGHW3X4FRW0FqNlbOi9sUYeBfFMnLePx1Gjfqn8MgS")
-     }
-    //headers:{"authorization": "Bearer c.s7K5ndoFFMrwvCbFypU2U29sRxlTQZ4jSJTvWD6zHLOxuRJjznVkm38MJi204sQrJnFhvQn3pygTeoM8IFLpLZfQwSlXE47zzs6JrPzGHW3X4FRW0FqNlbOi9sUYeBfFMnLePx1Gjfqn8MgS"}
+    //headers: { 'authorization': authToken}
     
     }).then(function (response)
     {
