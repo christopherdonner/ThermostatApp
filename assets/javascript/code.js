09d19168eeea51ba9c4ce12bf548da88d57aad8f
christopherdonner <christopher.donner@gmail.com>
2019-01-08 21:31:50 -0500

    all AJAX calls working

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,13 +1,31 @@
-var authToken="c.s7K5ndoFFMrwvCbFypU2U29sRxlTQZ4jSJTvWD6zHLOxuRJjznVkm38MJi204sQrJnFhvQn3pygTeoM8IFLpLZfQwSlXE47zzs6JrPzGHW3X4FRW0FqNlbOi9sUYeBfFMnLePx1Gjfqn8MgS"
+var thermostatID="uks8vKYvLFpURdo8n8GzwpNxir2Vn9sC"
 
+var nestURL="https://developer-api.nest.com"
+var nestAuthToken="c.s7K5ndoFFMrwvCbFypU2U29sRxlTQZ4jSJTvWD6zHLOxuRJjznVkm38MJi204sQrJnFhvQn3pygTeoM8IFLpLZfQwSlXE47zzs6JrPzGHW3X4FRW0FqNlbOi9sUYeBfFMnLePx1Gjfqn8MgS"
+
+var weatherURL="https://api.openweathermap.org/data/2.5/weather?"
+var weatherKey="586717aa0716809b69e439ab59109b93"
+
+//NEST thermostat API AJAX call 
 $.ajax({
-    url: `https://cors-escape.herokuapp.com/https://developer-api.nest.com/?auth=${authToken}`,
+    url: `https://cors-escape.herokuapp.com/${nestURL}/?auth=${nestAuthToken}`,
     type: "GET",
     contentType: "application/json",
-    
-    //headers: { 'Origin': }
-    
     }).then(function (response)
     {
         console.log(response)
+        console.log(response.devices.thermostats.uks8vKYvLFpURdo8n8GzwpNxir2Vn9sC.ambient_temperature_c)
+        console.log(thermostatID)
+
     })
+
+//OpenWeather AJAX call
+$.ajax({
+    url: `${weatherURL}q=Toronto&appid=${weatherKey}`,
+    type: "GET"
+}).then(function (response)
+{
+    console.log(response)
+    console.log(response.main.temp)
+}
+)
