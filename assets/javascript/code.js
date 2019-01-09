a6b4a813669617034342e8c1a62481a93ceca34c
christopherdonner <christopher.donner@gmail.com>
2019-01-08 20:25:08 -0500

    CORS issue circumvented!

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,10 +1,11 @@
 var authToken="c.s7K5ndoFFMrwvCbFypU2U29sRxlTQZ4jSJTvWD6zHLOxuRJjznVkm38MJi204sQrJnFhvQn3pygTeoM8IFLpLZfQwSlXE47zzs6JrPzGHW3X4FRW0FqNlbOi9sUYeBfFMnLePx1Gjfqn8MgS"
 
 $.ajax({
-    url: `https://developer-api.nest.com/?auth=${authToken}`,
+    url: `https://cors-escape.herokuapp.com/https://developer-api.nest.com/?auth=${authToken}`,
     type: "GET",
     contentType: "application/json",
-    //headers: { 'authorization': authToken}
+    
+    //headers: { 'Origin': }
     
     }).then(function (response)
     {
